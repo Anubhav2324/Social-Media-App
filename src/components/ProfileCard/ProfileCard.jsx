a389ab74e055d41497b5f2c105368653c8f21e84
Anubhav2324 <kumaranubhav8844@gmail.com>
2022-06-19 03:07:18 +0530

    Initial Commit Frontend

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './ProfileCard.css'
+import Cover from '../../img/cover.jpg';
+import Profile from '../../img/profileImg.jpg';
+
+const ProfileCard = () => {
+  return (
+    <div className='ProfileCard'>
+        <div className="ProfileImg">
+            <img src={Cover} alt="" />
+            <img src={Profile} alt="" />
+        </div>
+        <div className="ProfileName">
+            <span>Zendaya</span>
+            <span>Reporter</span>
+        </div>
+        <div className="FollowStatus">
+            <hr />
+            <div>
+                <div className="follow">
+                    <span>6,890</span>
+                    <span>Following</span>
+                </div>
+                <div className='vl'></div>
+                <div className="follow">
+                    <span>1</span>
+                    <span>Followers</span>
+                </div>
+            </div>
+            <hr />
+        </div>
+
+        <span>
+            My Profile
+        </span>
+
+    </div>
+  )
+}
+
+export default ProfileCard;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../img/logo.png';
+import {UilSearch} from '@iconscout/react-unicons';
+import './Search.css'
+
+const Search = () => {
+  return (
+    <div className='Search'>
+        <img src={Logo} alt='' />
+        <div className="SearchInput">
+            <input type="text" placeholder='#Explore' />
+            <div className="SearchIcon">
+                <UilSearch />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
